ad24044de4457447c22758fc1288719963f66c65
jaeyeop.shim <elapid0212@naver.com>
2025-02-10 10:52:31 +0900

    버전 1.12 -> .1.13

--- src/main/java/com/test/logging/SizeRollingFileAppender.java
+++ src/main/java/com/test/logging/SizeRollingFileAppender.java
@@ -120,11 +120,19 @@ public class SizeRollingFileAppender extends FileAppender {
 
         // 최대 인덱스를 초과하면 가장 오래된 백업 파일 삭제
         if (nextIndex > maxBackupIndex) {
-            File oldestBackup = new File(generateBackupFilename(scheduledFilename, 1));
+            // 기존 백업 파일의 인덱스를 순차적으로 증가시키고, 가장 오래된 파일 삭제
+            for (int i = maxBackupIndex; i > 1; i--) {
+                File file = new File(generateBackupFilename(scheduledFilename, i));
+                if (file.exists()) {
+                    File target = new File(generateBackupFilename(scheduledFilename, i + 1));
+                    file.renameTo(target);
+                }
+            }
+            File oldestBackup = new File(generateBackupFilename(scheduledFilename, 2));
             if (oldestBackup.exists()) {
                 oldestBackup.delete();
             }
-            nextIndex = 1;
+            nextIndex = 2;
         }
 
         // Rename the current log file to the next available index
